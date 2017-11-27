c0398adc123fdc4210d6b9f94849d83ff2439d98
江培瑋 <jiangpeiwei@jiangpeiweide-MacBook-Pro.local>
2017-11-27 16:28:08 +0800

    Initial Commit

---
+++ Gotham City e book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gotham City e book
+//
+//  Created by 江培瑋 on 2017/11/27.
+//  Copyright © 2017年 江培瑋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
